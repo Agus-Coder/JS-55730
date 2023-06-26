4e0135c453cc986aebc9df7937e9f624783f98b2
August <93748858+Agus-Coder@users.noreply.github.com>
2023-06-26 17:00:52 +0000

    Se agrega archivos con comentarios sobre clase 0

---
+++ 00-introduccion_a_javascript_+_onboarding/comentarios_clase_cero.js
@@ -0,0 +1,13 @@
+// Slide 12, mostrar un sitio estatico
+
+// App, mostrar una app web
+
+// Ejemplo de front y back en un restaurant con su cocina
+
+// hablar sobre extensiones
+
+// contar ejemplo sobre un auto, su carroceria y motores internos, como si fuera un transformer
+
+// mostrar un algoritmo de suma, el mas sencillo
+
+// hablar sobre el TOP 10
