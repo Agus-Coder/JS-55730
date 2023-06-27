c71f62812ceddc3a789fa99fc56732bb2e80f339
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2023-06-26 22:42:07 -0300

    First class examples

--- 01-conceptos_generales_sintaxis_y_variables/03-variables_y_valores/variables.js
+++ 01-conceptos_generales_sintaxis_y_variables/03-variables_y_valores/variables.js
@@ -1,22 +1,29 @@
-// declarar variables
+// ------ Declaracion ------ //
 
 let alto
 let ancho
 let profundidad
 
-// asignacion de variables
+// ------ Asignacion ------ //
 
 alto = 12.6
 ancho = 22.3
 profundidad = 43.8
 
 // Las constantes se deben asignar cuando se declaran
-
+// No se pueden modificar
 const precioPintura = 10
 
 
+// ------ Operaciones Basicas ------ //
+
+let areaPared = alto*ancho
+let todasLasAreas = areaPared*4
+let total = todasLasAreas*precioPintura
 
 
+
+// ------ Otros Ejemplos ------ //
 let name = "Pintadores"
 let adj = "Espaciales"
 const espacio = " "
--- 01-conceptos_generales_sintaxis_y_variables/04-prompt_console_alert/pca.js
+++ 01-conceptos_generales_sintaxis_y_variables/04-prompt_console_alert/pca.js
@@ -1,1 +1,13 @@
-test
+// ------ Console log ------ //
+
+console.log("Exito!");
+
+// ------ Alert ------ //
+
+alert("Exito 2!")
+
+// ------ Prompt ------ //
+
+let example = prompt("Ingrese un dato o valor!")
+
+console.log(example);
